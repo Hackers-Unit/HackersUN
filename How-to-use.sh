d8eb712764ce67e8493389770c2ff3d8d2cc01e0
Hackers-Unit <76515729+Hackers-Unit@users.noreply.github.com>
2021-03-02 23:26:03 +0100

    Add files via upload
    You can install this on your termux or kali linux using this command 
    
    apt-get update && apt-get upgrade
    apt install python2
    git clone https://github.com/Hackers-Unit/HackersUN
    chmod +x hackers-uni-virus.py
    cd HackersUN
    python2 hackers-uni-virus.py

---
+++ How-to-use.sh
@@ -0,0 +1,55 @@
+echo -e "\e[31mHow to install and use Hackers United Virus Maker
+Note: Only for educational purposes
+
+Make sure you have internet connections on your Phone
+
+Use the command below to install HackersUN
+
+apt-get update && apt-get upgrade
+apt install python2
+git clone https://github.com/Hackers-Unit/HackersUN
+chmod +x hackers-uni-virus.py
+cd HackersUN
+python2 hackers-uni-virus.py
+
+After installing and starting the command 
+Step 1: Click on enter key to continue
+
+Step 2: Select the virus version you want to create e.g for Android, Windows or Mac 
+
+Step 3: Select the the virus you want 
+
+Step 4: Once it shows download the virus and done just click on CTRL C on your termux and click on enter key
+
+Step 5: It will take you to the file directory after that just type ls and you will see the virus you created.
+
+Step 6: Now you have to copy the virus to your phone storage directory so that you can share it
+
+To copy the virus to your phone storage directory you have to follow this step 
+
+Step 1: You have to choose the file you are coping it to on your phone storage directory but I am coping my own to dcim folder 
+
+Step 2: What is the name of the virus you created my own is elite.apk. To copy it to your phone storage directory follow step 3
+
+Step 3: Type this following command on your termux inside the HackersUN directory 
+
+Step 4: "cp elite.apk /storage/emulated/0/DCIM/"
+And hit enter key the virus will be copy to the dcim folder 
+
+Step 5: Exit out of the HackersUN directory using this command "cd .." and go to storage directory using this command "cd storage" after that go to the dcim folder using this command "cd dcim" and type ls then you see the elite.apk
+
+Step 6: Instead of Going through step 5 instructions you can go to your phone manager app and go to the dcim folder you will see the elite.apk 
+
+Thanks for using our program 
+If you have any questions or issue you can contact us with this following details 
+
+Website: https://hackersunited.great-site.net
+Email: hackers1united@gmail.com
+
+And don't also forget to join us and register on our website for more hacking lessons😁
+
+Hackers United!!!
+We are one💪💪
+
+
+\e[0m";
---
+++ README.sh
@@ -0,0 +1,3 @@
+echo -e "\e[31mEducation is an essential aspect of this tool so use it wisely.
+We are not responsible for any damage or atrocities of this program.
+Only for Educational purposes.\e[0m";
